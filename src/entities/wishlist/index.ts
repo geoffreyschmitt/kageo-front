83426c33c76f75019a19cb9adef85be30fad6622
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2025-10-24 17:06:34 +0200

    chore: fix type exports in WishlistForm and EditWishlistForm types

--- src/entities/wishlist/index.ts
+++ src/entities/wishlist/index.ts
@@ -1,5 +1,5 @@
 export { WISHLIST_CATEGORIES, DEFAULT_WISHLIST_SETTINGS } from './model/constants'
 export type { WishlistCategory } from './model/constants'
 export type { Wishlist, WishlistFormData } from './model/types'
-export type {WishlistForm } from './ui/WishlistForm'
-export type {TEditWishlistFormData, TEditWishlistForm, TEditWishlistValidationErrors } from './ui/EditWishlistForm.types'
+export type { WishlistForm } from './ui/WishlistForm'
+export type { TWishlistFormData, TWishlistForm, TWishlistValidationErrors } from './ui/WishlistForm.types'
